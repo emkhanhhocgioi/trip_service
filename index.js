ca5d158da27792d35d9ec3ff8c370845455bd470
unknown <hidrabula@gmail.com>
2025-08-17 20:43:41 +0700

    deployment change

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: ['http://localhost:3001','http://localhost:3000'],
+  origin: ['http://localhost:3001','http://localhost:3000','https://bus-ticket-fe.vercel.app'],
   credentials: true
 }));
 
